f8a08ea166c36f34e910e1f90398b9ede2f9e92e
BeauX <fire-fly_7@hotmail.com>
2024-10-30 20:37:02 -0700

    step 5 commit

--- exercises.py
+++ exercises.py
@@ -50,5 +50,20 @@ class Game:
                 print("Invalid move! Please choose an empty space on the board.")
 
     
+    def check_for_winner(self):
+        
+        winning_combinations = [
+            ['a1', 'a2', 'a3'], ['b1', 'b2', 'b3'], ['c1', 'c2', 'c3'],  
+            ['a1', 'b1', 'c1'], ['a2', 'b2', 'c2'], ['a3', 'b3', 'c3'],  
+            ['a1', 'b2', 'c3'], ['a3', 'b2', 'c1']                       
+        ]
+
+        for combo in winning_combinations:
+            if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] == self.turn:
+                self.winner = self.turn
+                return True
+        return False
+
+
 game_instance = Game()
 game_instance.play_game()
